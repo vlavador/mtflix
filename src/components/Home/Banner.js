18f7be72c8fa4eb71a74dcd7d4503e2cf8d3bfa6
Vince Lavador <vincedlavador@gmail.com>
2021-04-27 15:37:21 +0800

    adding banner content

--- src/components/Home/Banner.js
+++ src/components/Home/Banner.js
@@ -3,7 +3,10 @@ const Banner = () => {
         <section 
             className='home-banner' 
             style={{backgroundImage:`linear-gradient(to right, rgba(6, 84, 147, 0.8) 0%, rgba(6, 84, 147, 0) 100%),url(https://image.tmdb.org/t/p/w1400_and_h450_face/b0WmHGc8LHTdGCVzxRb3IBMur57.jpg`}}>
-           
+            <div className="container home-content">
+                <h2>Welcome, Viewers.</h2>
+                <h3>Find movies,TV Shows and people. Discover now.</h3>
+            </div>
         </section>
     );
 
